71ee029429ccdf769656c43e361cff2e0501c845
Jason Howe <trisynth@gmail.com>
2020-09-29 11:53:03 -0400

    Testing changes

--- src/composition/Card.test.js
+++ src/composition/Card.test.js
@@ -5,7 +5,7 @@ import Card from './Card';
 // This is the test case.
 it('Renders without crashing', () => {
   // Create an element to test the component in.
-  const div = document.createElement('div-a');
+  const div = document.createElement('div');
   // Render the component to that element.
   ReactDOM.render(<Card />, div);
   // Clean up the created element.
