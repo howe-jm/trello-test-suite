0edbc71f6f1ebc19240456fed1889fb7b0543ae5
Jason Howe <trisynth@gmail.com>
2020-09-29 11:54:21 -0400

    Testing changes

--- src/composition/List.test.js
+++ src/composition/List.test.js
@@ -7,7 +7,7 @@ it('Renders without crashing', () => {
   // Create an element to test the component in.
   const div = document.createElement('div');
   // Render the component to that element.
-  ReactDOM.render(<List />, div);
+  ReactDOM.render(<List key={list.id} header={list.header} cards={list.cardIds.map((id) => store.allCards[id])} />, div);
   // Clean up the created element.
   ReactDOM.unmountComponentAtNode(div);
 });
